d46c3bf05440d78af859e106c1c2fd245f866653
Jordan Morris <jthecybertinkerer@gmail.com>
2020-06-04 15:51:20 -0500

    Init commit

---
+++ SharpEcho.Recruiting.SpellChecker.Core/DictionaryDotComSpellChecker.cs
@@ -0,0 +1,24 @@
+﻿using SharpEcho.Recruiting.SpellChecker.Contracts;
+
+namespace SharpEcho.Recruiting.SpellChecker.Core
+{
+    /// <summary>
+    /// This is a dictionary based spell checker that uses dictionary.com to determine if
+    /// a word is spelled correctly
+    /// 
+    /// The URL to do this looks like this: http://dictionary.reference.com/browse/<word>
+    /// where <word> is the word to be checked
+    /// 
+    /// Example: http://dictionary.reference.com/browse/SharpEcho would lookup the word SharpEcho
+    /// 
+    /// We look for something in the response that gives us a clear indication whether the
+    /// word is spelled correctly or not
+    /// </summary>
+    public class DictionaryDotComSpellChecker : ISpellChecker
+    {
+        public bool Check(string word)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SharpEcho.Recruiting.SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+using SharpEcho.Recruiting.SpellChecker.Contracts;
+using SharpEcho.Recruiting.SpellChecker.Core;
+
+namespace SharpEcho.Recruiting.SpellChecker.Tests
+{
+    [TestFixture]
+    class MnemonicSpellCheckerIBeforeETests
+    {
+        private ISpellChecker SpellChecker;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            SpellChecker = new MnemonicSpellCheckerIBeforeE();
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Correctly()
+        {
+            // implement this test
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Incorrectly()
+        {
+            // implement this test
+        }      
+    }
+}
